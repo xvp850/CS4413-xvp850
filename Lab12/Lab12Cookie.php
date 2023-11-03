f29a2fe9eaa6a7f11696cecf82813ed82dc8f1ad
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 12:30:05 -0500

    Add files via upload

---
+++ Lab12/Lab12Cookie.php
@@ -0,0 +1,23 @@
+<?php
+$cookieName = "userTest";
+$cookieValue = "Jane Doe";
+setcookie($cookieName, $cookieValue, time() + (60), "/");
+?>
+<html>
+    <head>
+        <title id="indexTitle">Lab 12</title>
+        <h1 id="indexh1">Lab 12 - Lab12Cookie.php</h1>
+        <p class="indexName">John H. McCann IV (xvp850)</p>
+    </head>
+    <body>
+<?php
+if(!isset($_COOKIE[$cookieName])) {
+  echo "Cookie named '" . $cookieName . "' is not set."; //userTest
+} else {
+  echo "Cookie '" . $cookieName . "' is set.<br>"; //userTest
+  echo "Value is: " . $_COOKIE[$cookieName]; //Display name
+}
+?>
+    </body>
+</html> 
+
