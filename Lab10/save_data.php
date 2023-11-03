b160aa7ab69c46a596e87473483d37e8eb2db23c
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 13:06:52 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -3,14 +3,16 @@ $myfile = fopen("../Lab10Data.txt", "w") or die("Unable to open file!");
 
 if(isset($_POST['create'])){
     $fullName = $_POST['fname']." ".$_POST[‘lname’]."\n";
-    //echo "Hello ".$_POST[‘fname’]." ".$_POST[‘lname’]."!\n";
-    //echo $fullname;
+    echo "Hello ".$_POST[‘fname’]." ".$_POST[‘lname’]."!\n";
+    echo $fullname;
     fwrite($myfile, $fullName);
     echo "Data saved successfully.";
+} else {
+    echo "Data did not save successfully."
 }
 
 fclose($myfile);
 
-echo <a href="Lab10index.html">Index Page</a>;
-echo <a href="readFile.php">Display file</a>;
+//echo <a href="Lab10index.html">Index Page</a>;
+//echo <a href="readFile.php">Display file</a>;
 ?> 
