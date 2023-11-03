e106cea886e896e4b3f5674201b5b38598029147
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 12:48:54 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -5,12 +5,10 @@ if(isset($_POST['create'])){
     $fullName = $_POST['fname']." ".$_POST[‘lname’]."\n";
     //echo "Hello ".$_POST[‘fname’]." ".$_POST[‘lname’]."!\n";
     //echo $fullname;
+    fwrite($myfile, $fullName);
+    echo "Data saved successfully.";
 }
 
-//echo "Hello ".$_POST[‘fname’]." ".$_POST[‘lname’]."!";
-
-//$txt = .$_POST[‘fname’]." ".$_POST[‘lname’]."\n";
-fwrite($myfile, $fullName);
 fclose($myfile);
 
 echo <a href="Lab10index.html">Index Page</a>;
