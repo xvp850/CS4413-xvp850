2324f4fb0fc7d5efecfa4d5f1f527a18dfab49ec
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 22:41:38 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -4,13 +4,16 @@ $myfile = fopen("Lab10Data.txt", "w") or die("Unable to open file!");
 if(isset($_POST['saveName'])){
     $fullName = $_POST['fname']." ".$_POST[‘lname’]."\n";
     fwrite($myfile, $fullName);
-    echo "Data saved successfully.";
+    echo 'Data saved successfully.';
 } else {
-    echo "Data did not save successfully."
+    echo 'Data did not save successfully.'
 }
 
 fclose($myfile);
-
-echo "<a href="Lab10index.html">Index Page</a>";
-echo "<a href="readFile.php">Display file</a>";
+echo '<form action="Lab10index.html">';
+echo '<input type="submit" value="Go Back to Index"/></form>'
+echo '<form action="readFile.php">';
+echo '<input type="submit" value="Read File"/></form>'
+//echo '<a href="Lab10index.html">Index Page</a>';
+//echo '<a href="readFile.php">Display file</a>';
 ?> 
