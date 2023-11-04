a54274697d8e0a1ed108b25a89802bded25973e3
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 22:16:34 -0500

    Update readFile.php

--- Lab10/readFile.php
+++ Lab10/readFile.php
@@ -1,10 +1,10 @@
 <?php 
-/*echo "<table width="200" border="1">";
+echo "<table width="200" border="1">";
 echo "<tr>";
 echo "<td width="85">First Name</td>";
 echo "<td width="99">Last Name</td>";
 echo "</tr>";
-
+/*
 $file = "Lab10Data.txt";
 
 if(file_exists($file)) {
@@ -21,14 +21,26 @@ if(file_exists($file)) {
     echo "No such file exists."
 }
 echo "<a href="Lab10index.html">Index Page</a>";*/
+echo '<head>';
+echo '<title>Lab 10 - readFile.php</title>';
+echo '<h1>Lab 10 - readFile.php</h1>';
+echo '<p>Name: John H. McCann IV (xvp850)</p><br/>';
+echo '</head>';
 
 $file = 'Lab10Data.txt';
 if(file_exists($file)) {
     $fileData = file($file);
+    echo "<table width="200" border="1">";
+    echo "<tr>";
+    echo "<td width="85">First Name</td>";
+    echo "<td width="99">Last Name</td>";
+    echo "</tr>";
     foreach($fileData as $fileLine) {
         $name = explode(' ', $fileLine);
-        echo $name[0];
+        echo '<tr><td>'.$name[0].'</td></tr>';
+        echo '<tr><td>'.$name[1].'</td></tr>';
     }
+    echo '</table>';
 } else {
     echo 'File does not exist.';
 }    
