4b23ba03798bfb60ab5075277ab0a49331558653
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 23:14:50 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,15 +1,13 @@
 <?php
-echo '<head>';
-echo '<title>Lab 10 - readFile.php</title>';
-echo '<h1>Lab 10 - readFile.php</h1>';
-echo '<p>Name: John H. McCann IV (xvp850)</p><br/>';
-echo '</head>';
+echo '<head><title>Lab 10 - save_data.php</title>';
+echo '<h1>Lab 10 - save_data.php</h1>';
+echo '<p>Name: John H. McCann IV (xvp850)</p><br/></head>';
 
 $myfile = 'Lab10Data.txt';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstName = $_POST['fname'] ?? '';
-    $lastName = $_POST['fname'] ?? '';
+    $lastName = $_POST['lname'] ?? '';
     $fullName = $firstName.' '.$lastName.'\n';
     //fwrite($myfile, $fullName);
     file_put_contents($myfile, $fullName, FILE_APPEND);
