f0ec20a512f25e15376293ca2469efd7a218763b
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 23:31:24 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -12,22 +12,16 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     //fwrite($myfile, $fullName);
     file_put_contents($myfile, $fullName, FILE_APPEND);
     echo 'Data saved successfully.';
-    echo '<form action="Lab10index.html">';
-    echo '<input type="submit" value="Go Back to Index"/></form>';
-    echo '<form action="readFile.php">';
-    echo '<input type="submit" value="Read File"/></form>';
+    echo buttonCall();
     fclose($myfile);
 } else {
     echo 'Invalid method. Data did not save successfully.';
-    echo '<form action="Lab10index.html">';
-    echo '<input type="submit" value="Go Back to Index"/></form>';
-    echo '<form action="readFile.php">';
-    echo '<input type="submit" value="Read File"/></form>';
+    echo buttonCall();
 }
 
 //fclose($myfile);
 function buttonCall() {
-    echo '<form action="Lab10index.html">';
+    echo '<br><form action="Lab10index.html">';
     echo '<input type="submit" value="Go Back to Index"/></form>';
     echo '<form action="readFile.php">';
     echo '<input type="submit" value="Read File"/></form>';
