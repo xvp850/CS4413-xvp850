2d149eb09944600b9913c628188e33a6230b80a2
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-03 23:19:41 -0500

    Update save_data.php

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -8,19 +8,19 @@ $myfile = 'Lab10Data.txt';
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstName = $_POST['fname'] ?? '';
     $lastName = $_POST['lname'] ?? '';
-    $fullName = $firstName.' '.$lastName."\n";
+    $fullName = $firstName.' '.$lastName.'\n';
     //fwrite($myfile, $fullName);
     file_put_contents($myfile, $fullName, FILE_APPEND);
     echo 'Data saved successfully.';
 } else {
-    echo 'Invalid method. Data did not save successfully.'
+    echo 'Invalid method. Data did not save successfully.';
 }
 
 fclose($myfile);
 echo '<form action="Lab10index.html">';
-echo '<input type="submit" value="Go Back to Index"/></form>'
+echo '<input type="submit" value="Go Back to Index"/></form>';
 echo '<form action="readFile.php">';
-echo '<input type="submit" value="Read File"/></form>'
+echo '<input type="submit" value="Read File"/></form>';
 //echo '<a href="Lab10index.html">Index Page</a>';
 //echo '<a href="readFile.php">Display file</a>';
 ?> 
